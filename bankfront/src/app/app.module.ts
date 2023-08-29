a079c02720e57497092e5d5fb88dbb50dfb8633b
flitzso <flitzcode@hotmail.com>
2023-08-29 15:46:04 -0300

    v1-2

---
+++ bankfront/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [], // Adicione o serviço aqui
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
